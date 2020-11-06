fa5f28f011dd2b71c18a382e8307215642e5ea91
mansour <mansour@web-id.fr>
2020-11-06 11:16:54 +0100

    app

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Contact.css'
+
+const Contact = () => {
+    return ( 
+        <div className="Contact">
+     <img className="avatar" alt="name" src ='https://i.ibb.co/4S9wSTD/te-le-chargement.jpg'/>
+    <div className="status">
+    <h4 className="name" >James Sweeney</h4>
+    <p className="status-text status-online">"Online"</p>
+    </div>
+  </div>
+     );
+}
+ 
+export default Contact;
